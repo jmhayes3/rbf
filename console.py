7193c40bff007de3283bb6de571f072460bada24
jmhayes3 <22490346+jmhayes3@users.noreply.github.com>
2023-04-20 22:39:47 -0500

    Update console.py

--- console.py
+++ console.py
@@ -1,17 +1,23 @@
-#!/usr/bin/env python3
-
-import logging
 import click
 
 from engine.manager import Manager
 
 
-@click.command()
-@click.option("--workers", "-w", default=1, help="Number of worker processes.")
-def cli(workers):
-    manager = Manager(num_workers=workers)
-    manager.start()
+@click.group()
+def engine():
+    pass
+
+
+@engine.command()
+@click.option("--workers", "-w", default=1, help="Number of workers to spawn.")
+@click.argument("address", nargs=1, type=click.Path())
+def start(workers, address):
+    click.echo(f"Num workers: {workers}")
+    click.echo(f"Address: {address}")
+
+    # manager = Manager(num_workers=workers)
+    # manager.start()
 
 
 if __name__ == "__main__":
-    cli()
+    engine()
