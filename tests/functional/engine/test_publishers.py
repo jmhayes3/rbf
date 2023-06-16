4fba5e8476bba1d6385a9abfe1610824c59092ec
jmhayes3 <22490346+jmhayes3@users.noreply.github.com>
2023-06-15 21:49:29 -0500

    Remove windows from build again.

--- tests/functional/engine/test_publishers.py
+++ tests/functional/engine/test_publishers.py
@@ -2,7 +2,8 @@ from rbf.engine.publishers import submission_publisher, comment_publisher
 
 
 def test_submission_publisher():
-	pass
+    pass
+
 
 def test_comment_publisher():
-	pass
+    pass
--- tests/functional/test_web.py
+++
@@ -1,8 +0,0 @@
-def test_main_route(test_client):
-    response = test_client.get("/")
-    assert response.status_code == 302
-
-
-def test_auth_route(test_client):
-    response = test_client.get("/auth/login")
-    assert response.status_code == 200
